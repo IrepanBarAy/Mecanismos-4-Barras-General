2ea58caf8bae69c199df2b25c0b651ff65388754
Irepan <irepanbaray@gmail.com>
2016-10-25 10:59:14 -0500

    Primer commit, inicializado el programa con suma de momentos en G3, inicializadas variables independientes

---
+++ SumMomG3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+main()
+{
+	double r1, r2, r3, r4, theta_1, theta_2, degtorad;
+	degtorad=pi/180;
+	r1=0;
+	r2=0;
+	r3=0;
+	r4=0;
+	theta_1=0;
+	for(theta_2=0;theta_2<361;theta
