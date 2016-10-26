e8bfc5be0a45fca2bd36da098444d040dac2312e
Irepan <irepanbaray@gmail.com>
2016-10-26 14:28:30 -0500

    Arreglados problemas con el cálculo de theta 4

--- SumMomG3.c
+++ SumMomG3.c
@@ -1,31 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <malloc.h>
 
-main()
+int main()
 {
 	/*Declaración de Variables*/
-	double r1, r2, r3, r4, theta_1, theta_2, degtorad, theta_ini, theta_fin, val;
-	degtorad=pi/180;
+	int cont;
+	double r1, r2, r3, r4, theta1, theta2, degtorad, theta_ini, theta_fin, val;
+	degtorad=3.141592654/180;
 	/*Oscilación de theta_2*/
 	theta_ini=60;
-	theta_fin=60;
-	val=(theta_fin-theta_ini)+1;
+	theta_fin=61;
+	val=(theta_fin-theta_ini);
 	/*Variables independientes conocidas*/
 	r1=0.9;
 	r2=0.3;
 	r3=1.5;
 	r4=0.8;
-	theta_1=0;
+	theta1=0;
 	/*Variables dependientes de theta_2*/
-	double theta_3[val], theta_4[val], J[val], K[val], L[val];
-	for(theta_2=theta_ini*degtorad;theta_2<theta_fin*degtorad+1;theta_2++)
+	double *theta3, *theta4, *J, *K, *L;
+	/*Declaración de punteros*/
+	theta3=(double *) malloc(sizeof(double) * val);
+	theta4=(double *) malloc(sizeof(double) * val);
+	J=(double *) malloc(sizeof(double) * val);
+	K=(double *) malloc(sizeof(double) * val);
+	L=(double *) malloc(sizeof(double) * val);
+	/*Ciclo para determinar theta_3 y theta_4*/
+	for(cont=0;cont<val+1;cont++)
 	{
-		J[theta_2]=2*r1*r4*cos(theta_1)-2*r2*r4*cos(theta_2);
-		K[theta_2]=2*r1*r4*sin(theta_1)-2*r2*r4*sin(theta_1);
-		L[theta_2]=r1*r1+r2*r2+r4*r4-r3*r3-2*r1*r2*(cos(theta_1)*cos(theta_2)+sin(theta_1)*sin(theta_2));
-	theta_4[theta_2]=2*arctan((-K[theta_2]-sqrt(K[theta_2]*K[theta_2]-L[theta_2]*L[theta_2]+J[theta_2]*J[theta_2]))/(L[theta_2]-J[theta_2]));
+		theta2=(theta_ini+cont)*degtorad;
+		J[cont]=2*r1*r4*cos(theta1)-2*r2*r4*cos(theta2);
+		K[cont]=2*r1*r4*sin(theta1)-2*r2*r4*sin(theta2);
+		L[cont]=r1*r1+r2*r2+r4*r4-r3*r3-2*r1*r2*(cos(theta1)*cos(theta2)+sin(theta1)*sin(theta2));
+		theta4[cont]=2*atan((-K[cont]-sqrt(K[cont]*K[cont]-L[cont]*L[cont]+J[cont]*J[cont]))/(L[cont]-J[cont]));
 	}
-	printf("theta_4: %f\n", theta_4[1]);
+	printf("theta_4: %f\n", theta4[0]/degtorad);
+	free(theta3);
+	free(theta4);
+	free(J);
+	free(K);
+	free(L);
+	system("pause");
 	return 0;
 }
